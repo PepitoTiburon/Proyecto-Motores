cc9a5cc860e389b88a2c7c95050e4221b91324d1
Dante <dantemolder@gmail.com>
2026-05-11 17:21:14 -0300

    primer commit

---
+++ mesh_instance_3d.gd
@@ -0,0 +1,12 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		self.queue_free()
