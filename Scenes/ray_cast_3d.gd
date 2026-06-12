22ac27df813a45ef8e9def3a47698a5df8b45520
Dante <dantemolder@gmail.com>
2026-06-11 23:17:17 -0300

    los objetos interactuables ahora son sostenibles

--- Scenes/ray_cast_3d.gd
+++ Scenes/ray_cast_3d.gd
@@ -1,6 +1,5 @@
 extends RayCast3D
 signal hasSidoInteractuado
-signal hasSidoInteractuado2
 var ObjetoObservado:Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,7 +11,4 @@ func _process(_delta: float) -> void:
 	ObjetoObservado = get_collider()
 	if ObjetoObservado != null:
 		if ObjetoObservado.is_in_group("interactuable") && Input.is_action_just_pressed("Interact"):
-			if ObjetoObservado == $"../../../../TestObjetoInteractuable2":
-				emit_signal("hasSidoInteractuado2")
-			if ObjetoObservado == $"../../../../StaticBody3D":
-				emit_signal("hasSidoInteractuado")
+			hasSidoInteractuado.emit(ObjetoObservado)
--- Scenes/test_objeto_interactuable_2.gd
+++ Scenes/test_objeto_interactuable_2.gd
@@ -1,19 +1,16 @@
 extends StaticBody3D
+
 var Encendido:bool 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Encendido = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func _on_ray_cast_3d_has_sido_interactuado_2() -> void:
-	if !Encendido:
-		Encendido = true
-		$SpotLight3D.light_energy = 9
-	elif Encendido:
-		Encendido = false
-		$SpotLight3D.light_energy = 0
+func _on_ray_cast_3d_has_sido_interactuado(objeto) -> void:
+	if objeto == self:
+		if !Encendido:
+			Encendido = true
+			$SpotLight3D.light_energy = 9
+		elif Encendido:
+			Encendido = false
+			$SpotLight3D.light_energy = 0
