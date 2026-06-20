4889b67bfe2d9f841ee79f7caef4db917de323b6
Dante <dantemolder@gmail.com>
2026-06-20 19:34:40 -0300

    añadi texturas a cada objeto que no las tenia, añadi un sonido de serrado a ambas puertas del pasadizo secreto y arregle un bug que hacia que si intentas abrir la puerta repetidas veces durante la misma animacion quedaba en diagonal

--- scenes/objects/door/class_puerta.gd
+++ scenes/objects/door/class_puerta.gd
@@ -1,17 +1,29 @@
 class_name Puerta
 extends ObjetoInteractuable
 @export var bloqueada:bool = false
+@export var CloseSound:RaytracedAudioPlayer3D
 var esta_abierta: bool = false
+var abriendo:bool = false
+var tween:Tween
 var angulo_apertura: float = deg_to_rad(90) # Convierte 90 grados a radianes
 
 func react() -> void:
-	var tween = create_tween()
 
-	if not esta_abierta && !bloqueada:
+	if not esta_abierta && !bloqueada && !abriendo:
 		# Rota a 90 grados en el eje Y
-		tween.tween_property(self, "rotation:y", rotation.y + angulo_apertura, 0.5)
+		abriendo=true
 		esta_abierta = true
-	elif !bloqueada && esta_abierta:
+		tween = create_tween()
+		tween.tween_property(self, "rotation:y", rotation.y + angulo_apertura, 0.5)
+		await tween.finished
+		abriendo=false
+	elif !bloqueada && esta_abierta && !abriendo:
 		# Vuelve a la posición original
-		tween.tween_property(self, "rotation:y", rotation.y - angulo_apertura, 0.5)
+		abriendo = true
 		esta_abierta = false
+		tween = create_tween()
+		tween.tween_property(self, "rotation:y", rotation.y - angulo_apertura, 0.5)
+		await tween.finished
+		if CloseSound != null:
+			CloseSound.play()
+		abriendo=false
