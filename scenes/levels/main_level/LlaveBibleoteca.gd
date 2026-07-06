6d1217fb8a1faa686e813b572ce69f4394fa0b2b
PepitoTiburon <dantemolder@gmail.com>
2026-07-06 01:51:11 -0300

    la llave de la bibleoteca ahora desbloquea la enfermeria y la llave del sotano ahora se ve como una llave

--- scenes/levels/main_level/LlaveBibleoteca.gd
+++ scenes/levels/main_level/LlaveBibleoteca.gd
@@ -4,4 +4,5 @@ extends ObjetoInteractuable
 # Called when the node enters the scene tree for the first time.
 func react() -> void:
 	$"../../../../Puerta".bloqueada = false
+	$"../../../../Puerta A Enfermeria".bloqueada = false
 	self.queue_free()
